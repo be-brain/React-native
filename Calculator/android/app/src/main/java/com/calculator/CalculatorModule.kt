f064fbd086a2d40b6f56f9b4a0e98697f993d255
be-brain <chha5232@gmail.com>
2023-10-23 22:00:47 +0900

    feat : native module 설정

---
+++ Calculator/android/app/src/main/java/com/calculator/CalculatorModule.kt
@@ -0,0 +1,30 @@
+package com.calculator
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class CalculatorModule(reactContext: ReactApplicationContext):ReactContextBaseJavaModule(reactContext){
+    override fun getName()="CalculatorModule"
+    @ReactMethod
+    fun executeCalc(action:String, numberA:Double, numberB:Double, promise:Promise){
+        if(action === "plus"){
+            promise.resolve(numberA+numberB);
+            return;
+        }
+        if(action === "minus"){
+            promise.resolve(numberA-numberB);
+            return;
+        }
+        if(action === "multiply"){
+            promise.resolve(numberA*numberB);
+            return;
+        }
+        if(action === "divide"){
+            promise.resolve(numberA/numberB);
+            return;
+        }
+        promise.reject("Unexpected action type")
+    }
+}
